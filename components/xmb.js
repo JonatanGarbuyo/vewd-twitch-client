3ca6562b368c430ac280be84124bbfa751c15746
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-05-08 18:41:27 -0300

    fix  error reading map of undefined

--- components/xmb.js
+++ components/xmb.js
@@ -4,9 +4,9 @@ export default function Xmb({ data, Card }) {
   return (
     <div className={styles.xmb}>
       <div className={styles.row}>
-        {data.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        {data.length
+          ? data.map((item) => <Card item={item} key={item.id} />)
+          : null}
       </div>
     </div>
   )

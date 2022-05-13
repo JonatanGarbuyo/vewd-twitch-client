a604975ecc8ff094648faeb6ed7921816bd0cc34
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-05-13 20:17:02 -0300

    add placeholder data

--- components/xmb.js
+++ components/xmb.js
@@ -1,12 +1,25 @@
 import styles from './xmb.module.css'
 
+const placeholderData = [
+  {
+    id: '321',
+    user_id: '',
+    user_login: '',
+    user_name: '',
+    game_name: '',
+    title: '',
+    thumbnail_url: '/',
+    box_art_url: '/',
+  },
+]
+
 export default function Xmb({ data, Card }) {
   return (
     <div className={styles.xmb}>
       <div className={styles.row}>
         {data.length
           ? data.map((item) => <Card item={item} key={item.id} />)
-          : null}
+          : placeholderData.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   )

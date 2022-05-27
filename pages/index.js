e5d5dc83f955d50e131ed7a6c8aa415227a86578
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-05-26 22:57:05 -0300

    refactor categories navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 import CategoryCard from '../components/categoryCard'
 
 import ChannelCard from '../components/channelCard'
+import DirectoryCard from '../components/directoryCard'
 import DirectoryNav from '../components/directoryNav'
 import Nav from '../components/nav'
 import Xmb from '../components/xmb'
@@ -12,7 +14,31 @@ import getTopGames from '../lib/api/getTopGames'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ topGames }) {
-  const { streams } = useTwitch()
+  const { streams, categories } = useTwitch()
+
+  function listener(e) {
+    console.log(e.keyCode)
+    if (e.keyCode == 39) {
+      e.preventDefault()
+      const next =
+        e.srcElement.parentElement.nextElementSibling?.firstElementChild
+      next && next.focus()
+    }
+    if (e.keyCode == 37) {
+      e.preventDefault()
+      const prev =
+        e.srcElement.parentElement.previousElementSibling?.firstElementChild
+      prev && prev.focus()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', listener)
+
+    return () => {
+      document.removeEventListener('keydown', listener)
+    }
+  })
 
   return (
     <>
@@ -40,8 +66,9 @@ export default function Home({ topGames }) {
           </div>
 
           <Xmb data={topGames} Card={CategoryCard} />
+          <Xmb data={categories} Card={DirectoryCard} />
 
-          <DirectoryNav />
+          {/* <DirectoryNav /> */}
 
           <div className={styles.recomended}>
             <div>

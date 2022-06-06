92f0477ede06e222e514ff82245b61c2adf59d40
Jonatan Garbuyo <jonatangarbuyo@gmail.com>
2022-06-05 21:07:01 -0300

    add api endopint for streams by category

--- pages/api/streams.js
+++ pages/api/streams.js
@@ -1,14 +1,26 @@
-import getStreams from '../../lib/api/getStreams'
+import getStreams, { getStreamsByCategory } from '../../lib/api/getStreams'
 
 export default async function streams(req, res) {
+  const { categoryId } = req.query
+
   if (req.method === 'GET') {
-    try {
-      const data = await getStreams()
-      // console.log('--streams: ', data)
-      res.status(200).send(data)
-    } catch (error) {
-      console.error(error)
-      res.status(500).send(error)
+    if (categoryId) {
+      try {
+        const data = await getStreamsByCategory(categoryId)
+        res.status(200).send(data)
+      } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+      }
+    } else {
+      try {
+        const data = await getStreams()
+        // console.log('--streams: ', data)
+        res.status(200).send(data)
+      } catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+      }
     }
   }
 }
